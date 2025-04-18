57c978a0e7fa26121e90074032f34125ac2592c0
danielatuesta55 <danielatuesta55@gmail.com>
2025-04-18 13:36:55 -0500

    Added changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,23 @@
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{js,jsx,ts,tsx}'
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
-      colors: { primary: '#0071e3' },
-      fontFamily: { 'sf-pro': ['-apple-system','BlinkMacSystemFont','"San Francisco Pro"','Segoe UI','Roboto','sans-serif'] },
+      colors: {
+        primary: '#0071e3',
+      },
+      fontFamily: {
+        'sf-pro': [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"San Francisco Pro"',
+          'Segoe UI',
+          'Roboto',
+          'sans-serif',
+        ],
+      },
     },
   },
   plugins: [],
