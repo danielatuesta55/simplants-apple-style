0875e43cfeb8a57bd701fcce3260fc22b0b3fd20
danielatuesta55 <danielatuesta55@gmail.com>
2025-04-18 14:29:32 -0500

    Added changes 2.29pm

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import './index.css';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+// src/main.jsx
+import './index.css' 
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
