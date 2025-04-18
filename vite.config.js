26656049f0248fb5db171fefe433b0e8934653a5
danielatuesta55 <danielatuesta55@gmail.com>
2025-04-18 14:51:51 -0500

    Added changes 2.51pm

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindPostcss from '@tailwindcss/postcss'
-import autoprefixer   from 'autoprefixer'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindPostcss(),  // ← the correct PostCSS plugin
-        autoprefixer(),
-      ]
-    }
-  }
 })
